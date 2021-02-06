c41803278b43ca007bb8d3280f894eb956617e6e
WillLoughlin <willdloughlin@gmail.com>
2021-02-05 19:27:19 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,278 @@
+//---------This code here used to set up Express (send index.html file to client)----------//
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/client/index.html');
+});
+
+app.use('/client', express.static(__dirname + '/client'));
+
+serv.listen(2000);
+console.log("Server Started");
+//--------------End of express setup code----------------//
+
+//import * as THREE from './client/js/three.js';
+
+//var THREE = require('./client/js/three.js');
+//var PointerLockControls = require('./client/js/PointerLockControls.js');
+
+//import { PointerLockControls } from './client/js/PointerLockControls.js';
+
+
+var SOCKET_LIST = {};
+var PLAYER_LIST = {};
+//var BLOCK_LIST = {};
+//var POINT_LIST = {};//3d
+
+var playerSpeed = 0.05;
+var playerHeight = 1;
+
+//var playerHeight = 100;
+var playerWidth = 50;
+
+// var blockWidth = 100;
+// var blockHeight = 100;
+// var numBlocks = 0;
+// var numPoints = 0;//3d
+
+//var direction = new THREE.Vector3();//used to save player x,z
+
+var Player = function(id){
+  var self = {
+    x:0,
+    y:0,
+    z:0,
+    thetaX:0,
+    thetaY:0,
+    thetaZ:0,
+    id:id,
+    width:playerWidth,
+    height:playerHeight,
+    name:"Will",
+    number:""+Math.floor(10*Math.random()),
+    cameraX:0,
+    cameraY:0,
+    pressingRight:false,
+    pressingLeft:false,
+    pressingUp:false,
+    pressingDown:false,
+    arrowUp:false,
+    arrowDown:false,
+    arrowRight:false,
+    arrowLeft:false,
+    maxSpd:0.3,
+    viewSpd:0.05,
+    verticalSpeed:0,
+    maxVerticalSpeed:10
+  }
+  self.updatePosition = function(){
+    if (self.pressingRight){
+      self.x = self.x + self.maxSpd * Math.sin(self.thetaY + 1.5708); //adding 90 degrees in radians
+      self.z = self.z + self.maxSpd * Math.cos(self.thetaY + 1.5708);
+    }
+    if (self.pressingLeft){
+      self.x = self.x - self.maxSpd * Math.sin(self.thetaY + 1.5708);
+      self.z = self.z - self.maxSpd * Math.cos(self.thetaY + 1.5708);
+    }
+    if(self.verticalUp) {
+      self.y = self.y + self.maxSpd;
+    }
+    if(self.verticalDown) {
+      self.y = self.y - self.maxSpd;
+    }
+    if(self.pressingUp) {
+      self.z = self.z + self.maxSpd * Math.cos(self.thetaY);
+      self.x = self.x + self.maxSpd * Math.sin(self.thetaY);
+      //console.log(self.z);
+    }
+    if(self.pressingDown) {
+      self.z = self.z - self.maxSpd * Math.cos(self.thetaY);
+      self.x = self.x - self.maxSpd * Math.sin(self.thetaY);
+      //console.log(self.z);
+    }
+    // if(self.arrowUp) {
+    //   self.thetaX = self.thetaX - self.viewSpd;
+    // }
+    // if(self.arrowDown) {
+    //   self.thetaX = self.thetaX + self.viewSpd;
+    // }
+    // if(self.arrowLeft) {
+    //   self.thetaY = self.thetaY - self.viewSpd;
+    // }
+    // if(self.arrowRight) {
+    //   self.thetaY = self.thetaY + self.viewSpd;
+    // }
+  }
+
+  // self.checkCollisionPlayerAllBlocks = function(){
+  //   for (var i in BLOCK_LIST){
+  //     var block = BLOCK_LIST[i];
+  //     if (self.checkCollisionPlayerBlock(block)){
+  //       return true;
+  //     }
+  //   }
+  //   return false;
+  // }//calls check single block fncn with all blocks
+  // self.checkCollisionPlayerBlock = function(block){
+  //   var selfXTL = self.x - (self.width/2);
+  //   var selfYTL = self.y - (self.height/2);
+  //   var bXTL = block.x - (block.width/2);
+  //   var bYTL = block.y - (block.height/2);
+  //
+  //   if (selfXTL < bXTL + block.width &&
+  //     selfXTL + self.width > bXTL &&
+  //     selfYTL < bYTL + block.height &&
+  //     selfYTL + self.height > bYTL) {
+  //       return true;
+  //   }
+  //   return false;
+  // }//checks player against single block
+  self.jump = function(){
+    self.verticalSpeed = -25;
+  }//function to make player jump
+
+  return self;
+}
+
+
+//Socket.io on connection and disconnection
+var io = require('socket.io')(serv,{});
+//main io connection and key detection function
+io.sockets.on('connection', function(socket){//called when player connects with server
+  socket.id = Math.random();
+  SOCKET_LIST[socket.id] = socket;//adding player socket to list
+  //console.log('socket connection');
+
+  var player = Player(socket.id);
+  console.log("New Player with ID: " + socket.id);
+  PLAYER_LIST[socket.id] = player;
+
+
+  SOCKET_LIST[socket.id].emit('setID', {id:socket.id, spd:playerSpeed, hgt:playerHeight});//emmitting id of player, used for centering camera
+
+  //this sends all other player information to new player just connected
+  var playerPack = [];
+  for (var i in PLAYER_LIST){
+      var player = PLAYER_LIST[i];
+      playerPack.push({
+        name:player.name,
+        x:player.x,
+        y:player.y,
+        z:player.z,
+        id:player.id
+      });
+  }
+  SOCKET_LIST[socket.id].emit('initPlayers', playerPack);//sending other player information to new player
+
+  //now we send new player information to all other new players
+  for(var i in SOCKET_LIST) {
+    if(i != socket.id){//this makes sure it doesnt send single new player info to the newly connected player (they get all info from initPlayers)
+      //console.log("sent new player with id: " + player.id + "information to player " + i);
+      SOCKET_LIST[i].emit('newPlayer',{id:socket.id,x:player.x,y:player.y,z:player.z,name:player.name});//player is new player, sending information to all others
+    }
+  }
+
+  socket.on('disconnect',function(){//called when player disconnects from server
+    console.log("Player Disconnected with ID: " + socket.id);
+    for(var i in SOCKET_LIST) {
+      if(i != socket.id){//this sends player disconnection info to all players
+        SOCKET_LIST[i].emit('disconnectedPlayer',{id:socket.id});//sending disconnect information to all others
+      }
+    }
+    delete SOCKET_LIST[socket.id];//deleting player's socket from list
+    delete PLAYER_LIST[socket.id];
+  });
+
+
+
+  //This function recieves player position and direction information from each player every cycle of gameloop
+  socket.on('selfMoveInfo',function(data){
+    player.cameraX = data[0].cameraX;
+    player.cameraY = data[0].cameraY;
+    player.x = data[0].playerX;
+    player.y = data[0].playerY;
+    player.z = data[0].playerZ;
+  });
+
+  socket.on('keyPress',function(data){//called when player presses a key
+    if(data.inputId === 'left'){
+      player.pressingLeft = data.state;
+    } else if (data.inputId === 'right'){
+      player.pressingRight = data.state;
+    } else if (data.inputId === 'up'){
+      player.pressingUp = data.state;
+    } else if (data.inputId === 'down'){
+      player.pressingDown = data.state;
+    } else if (data.inputId === 'jump'){
+      player.verticalUp = data.state;
+    } else if (data.inputId === 'shift'){
+      player.verticalDown = data.state;
+    } else if (data.inputId === 'arrowUp') {
+      player.arrowUp = data.state;
+    } else if (data.inputId === 'arrowDown') {
+      player.arrowDown = data.state;
+    } else if (data.inputId === 'arrowLeft') {
+      player.arrowLeft = data.state;
+    } else if (data.inputId === 'arrowRight') {
+      player.arrowRight = data.state;
+    }
+  });
+});
+
+
+setInterval(function(){//game loop
+  var pack = [];//pack to transfer data
+  for (var i in PLAYER_LIST){
+      var player = PLAYER_LIST[i];
+      player.updatePosition();
+      //console.log("Player " + i + " position: " + player.x + "," + player.z);
+      pack.push({
+        x:player.x,
+        y:player.y,
+        z:player.z,
+        maxSpd:player.maxSpd,
+        thetaX:player.thetaX,//camera angle in radians
+        thetaY:player.thetaY,
+        thetaZ:player.thetaZ,
+        id:player.id,
+        height:player.height,
+        width:player.width,
+        number:player.number,
+        isPlayer:true,
+        isBlock:false,
+        isPoint:false
+      });
+  }
+
+  // for (var i in BLOCK_LIST){
+  //   var block = BLOCK_LIST[i];
+  //   pack.push({
+  //     x:block.x,
+  //     y:block.y,
+  //     height:block.height,
+  //     width:block.width,
+  //     isPlayer:false,
+  //     isBlock:true,
+  //     isPoint:false
+  //   });
+  // }
+
+  // for (var i in POINT_LIST){
+  //   var point = POINT_LIST[i];
+  //   pack.push({
+  //     x:point.x,
+  //     y:point.y,
+  //     z:point.z,
+  //     isPoint:true,
+  //     isPlayer:false,
+  //     isBlock:false
+  //   });
+  // }
+
+  for (var i in SOCKET_LIST){
+    var socket = SOCKET_LIST[i];
+    socket.emit('gameLoop',pack);
+  }
+}, 1000/100);
---
+++ client/js/playerCode.js
@@ -0,0 +1,277 @@
+//Script that controls what happens player side
+/*
+Will Loughlin
+
+Each player recieves a copy of this file on connection.
+Movement is handled here and position is relayed to app.js server file
+Everytime gameloop is called this file recieves position and direction of each other player
+*/
+
+//Width and height of screen, will be changed when screen is sized
+var WIDTH = 500;
+var HEIGHT = 500;
+
+//View FOV
+var FOV_degrees = 60;
+
+var OTHER_PLAYER_LIST = {};
+
+var speed = 0.05;
+var height = 1;
+
+//These variables used for movement
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+let canJump = false;
+
+//more movement
+
+const velocity = new THREE.Vector3();
+const direction = new THREE.Vector3();
+const vertex = new THREE.Vector3();
+const color = new THREE.Color();
+
+//THREE.js functionality
+//https://www.youtube.com/watch?v=WRQey_qpJls&list=PLRtjMdoYXLf6mvjCmrltvsD0j12ZQDMfE&index=2&ab_channel=SonarSystems
+var scene = new THREE.Scene();//creating 3d scene
+var camera = new THREE.PerspectiveCamera(FOV_degrees,WIDTH/HEIGHT,0.1,1000);//creating camera, second two variables are distance not shown close and far
+
+var renderer = new THREE.WebGLRenderer();//initialize renderer (basically canvas)
+renderer.setSize(WIDTH,HEIGHT);//setting size to screen size
+document.body.appendChild(renderer.domElement);//adding renderer to document
+
+var geometry = new THREE.BoxGeometry(1,1,1);//width,depth,HEIGHT
+
+var socket = io();
+var selfID = 0;
+var indexSelf = 0;
+
+
+//Getting player's self ID
+socket.on('setID', function(playerID){
+  selfID = playerID.id;
+  console.log("New ID Set to: " + selfID);
+  speed = playerID.spd;
+  height = playerID.hgt;
+});
+
+
+//adding stuff to scene
+var cubeMaterials =
+[
+  new THREE.MeshBasicMaterial({map:new THREE.TextureLoader().load('client/img/1.JPG'),side: THREE.DoubleSide}),//right side
+  new THREE.MeshBasicMaterial({map:new THREE.TextureLoader().load('client/img/2.jpg'),side: THREE.DoubleSide}),//left side
+  new THREE.MeshBasicMaterial({map:new THREE.TextureLoader().load('client/img/3.jpg'),side: THREE.DoubleSide}),//top side
+  new THREE.MeshBasicMaterial({map:new THREE.TextureLoader().load('client/img/4.jpg'),side: THREE.DoubleSide}),//bottom side
+  new THREE.MeshBasicMaterial({map:new THREE.TextureLoader().load('client/img/5.jpg'),side: THREE.DoubleSide}),//front side
+  new THREE.MeshBasicMaterial({map:new THREE.TextureLoader().load('client/img/6.jpg'),side: THREE.DoubleSide}),//back side
+];//each image corresponds to different side
+
+//create a material,color, or image texture
+//var material = new THREE.MeshBasicMaterial({color: 0xFFFFFF,wireframe:true});
+var material = new THREE.MeshFaceMaterial(cubeMaterials);//call with image array
+var cube = new THREE.Mesh(geometry,material);//creating cube
+scene.add(cube);//adding cube to 3d scene
+
+camera.position.z = 3;//initialize camera position
+camera.position.y = height;
+
+
+
+const geometryCylinder = new THREE.CylinderGeometry( 0.25, 0.25, height, 32 );
+const materialCylinder = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+//cylinder = new THREE.Mesh( geometry, material );
+
+
+//----------------Drawing other players in game-----------------------//
+socket.on('initPlayers', function(players){
+  //console.log("creating players");
+  for(var i = 0; i < players.length; i++){
+      if(players[i].id == selfID){
+        indexSelf = i;//finding player index
+      }
+      var cylinder = new THREE.Mesh( geometryCylinder, materialCylinder );
+      //console.log("Player position: " + players[i].x + "," +players[i].z);
+      cylinder.position.x = players[i].x;
+      cylinder.position.y = players[i].y;
+      cylinder.position.z = players[i].z;
+      OTHER_PLAYER_LIST[players[i].id] = cylinder;
+      if(players[i].id != selfID){
+        //console.log("cylinder created at "+OTHER_PLAYER_LIST[other_player_count].position.x + ","+OTHER_PLAYER_LIST[other_player_count].position.z)
+        scene.add(OTHER_PLAYER_LIST[players[i].id]);
+      }
+      //console.log("other player count: " + other_player_count );
+      //other_player_count = other_player_count + 1;
+  }
+});
+//-------------------End drawing other players---------------------//
+
+//this function adds a new player to scene when other new player connects
+socket.on('newPlayer',function(newPlayer){
+  console.log("new player connected with id: " + newPlayer.id);
+  var newcylinder = new THREE.Mesh( geometryCylinder, materialCylinder );
+  newcylinder.position.x = newPlayer.x;
+  newcylinder.position.y = newPlayer.y;
+  newcylinder.position.z = newPlayer.z;
+  OTHER_PLAYER_LIST[newPlayer.id] = newcylinder;
+  scene.add(OTHER_PLAYER_LIST[newPlayer.id]);//adding new player to scene
+});
+
+socket.on('disconnectedPlayer',function(oldPlayer){
+  scene.remove(OTHER_PLAYER_LIST[oldPlayer.id]);
+  delete OTHER_PLAYER_LIST[oldPlayer.id];
+});
+
+//end adding stuff to scene
+
+
+//Function to resize canvas if screen size changes
+let resizeCanvas = function(){
+  WIDTH = window.innerWidth - 0;//needs - 0 otherwise html loads really slow for some reason
+  HEIGHT = window.innerHeight - 0;
+
+  renderer.setSize(WIDTH, HEIGHT);//resetting render image size
+  camera.aspect = WIDTH/HEIGHT;//fixing aspect ratio
+  camera.updateProjectionMatrix();//updating camera with new aspect ratio
+
+}
+resizeCanvas();//resize first time to get correct size
+
+window.addEventListener('resize',function(){//called if canvas size changes
+  resizeCanvas();
+});
+
+//controls from three
+const controls = new THREE.PointerLockControls(camera, document.body);
+
+//This locks the pointer, press esc to unlock
+document.addEventListener( 'click', function () {
+  controls.lock();//when screen is clicked mouse is locked in to view
+});
+
+
+scene.add(controls.getObject());//adding controls to scene
+
+
+//This function handles button inputs, will work for WASD or arrows
+document.onkeydown  = function ( event ) {//called when keys are pressed
+	switch ( event.code ) {
+	case 'ArrowUp':
+	case 'KeyW':
+		moveForward = true;//this will happen if you press w or uparrow
+    socket.emit('keyPress', {inputId:'right',state:true});
+		break;
+
+	case 'ArrowLeft':
+	case 'KeyA':
+		moveLeft = true;
+    socket.emit('keyPress', {inputId:'left',state:true});
+		break;
+
+	case 'ArrowDown':
+	case 'KeyS':
+		moveBackward = true;
+    socket.emit('keyPress', {inputId:'down',state:true});
+		break;
+
+	case 'ArrowRight':
+	case 'KeyD':
+		moveRight = true;
+    socket.emit('keyPress', {inputId:'right',state:true});
+		break;
+
+	case 'Space':
+		if ( canJump === true ) velocity.y += 350;
+		canJump = false;
+		break;
+	}
+};
+
+//called when keys are released, sets bools to false and stops movement
+document.onkeyup = function ( event ) {
+  switch ( event.code ) {
+	   case 'ArrowUp':
+	   case 'KeyW':
+		   moveForward = false;
+       socket.emit('keyPress', {inputId:'up',state:false});
+		   break;
+
+	   case 'ArrowLeft':
+	   case 'KeyA':
+		   moveLeft = false;
+       socket.emit('keyPress', {inputId:'left',state:false});
+		   break;
+
+	   case 'ArrowDown':
+	   case 'KeyS':
+		   moveBackward = false;
+       socket.emit('keyPress', {inputId:'down',state:false});
+		   break;
+
+	   case 'ArrowRight':
+	   case 'KeyD':
+	     moveRight = false;
+       socket.emit('keyPress', {inputId:'right',state:false});
+		   break;
+	 }
+};
+
+
+//------------------Main Drawing Loop----------------//
+var target = new THREE.Vector3();//used to save camera direction
+var oldX = 0;
+var oldY = 0;
+
+socket.on('gameLoop', function(data){
+  for(var i = 0; i < data.length; i++){
+    if (data[i].isPlayer){
+      //console.log("setting player " + i + " position to " + data[i].x + "," + data[i].z);
+      OTHER_PLAYER_LIST[data[i].id].position.x = data[i].x;
+      OTHER_PLAYER_LIST[data[i].id].position.y = data[i].y;
+      OTHER_PLAYER_LIST[data[i].id].position.z = data[i].z;
+      //player_counter += 1;
+      //console.log("counter = " + counter);
+    }
+  }
+
+
+  direction.z = Number( moveForward ) - Number( moveBackward );//1 if move forward, -1 if move backward
+	direction.x = Number( moveRight ) - Number( moveLeft );//1 if move right, -1 if move left
+  direction.normalize(); // this ensures consistent movements in all directions
+
+  controls.moveRight(speed * direction.x);
+  controls.moveForward(speed * direction.z);
+
+  sendPlayerInfo();
+  update();
+  render();
+
+});
+//---------------End of main drawing loop----------------------//
+
+//Send player info to server
+var sendPlayerInfo = function(){
+  var data = [];
+  camera.getWorldDirection(target);//direction is copied into vector "target"
+  data.push({
+    cameraX:target.x,
+    cameraY:target.y,
+    playerX:camera.position.x,
+    playerY:camera.position.y,
+    playerZ:camera.position.z
+  });
+  socket.emit('selfMoveInfo',data);
+};
+
+//game logic
+var update = function(){
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.005;
+};
+
+//draw scene
+var render = function() {
+  renderer.render(scene,camera);
+};
